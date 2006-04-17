45b24754e46079532df858a174b64edcc634c80f
Aaron Bockover <abockover@novell.com>
2006-04-17 16:33:39 +0000

    2006-04-17  Aaron Bockover  <aaron@abock.org>
        * src/NjbTest.cs: Added --hal-fdi-dump to generate HAL FDI entries for
        all devices listed in DeviceId
    
        * src/DeviceId.cs: Added support for storing input/output formats
    
    
    svn path=/trunk/njb-sharp/; revision=59542

--- src/DeviceId.cs
+++ src/DeviceId.cs
@@ -2,8 +2,8 @@
 /***************************************************************************
  *  DeviceId.cs
  *
- *  Copyright (C) 2005 Novell
- *  Written by Aaron Bockover (aaron@aaronbock.net)
+ *  Copyright (C) 2005-2006 Novell, Inc.
+ *  Written by Aaron Bockover <aaron@abock.org>
  ****************************************************************************/
 
 /*  THIS FILE IS LICENSED UNDER THE MIT LICENSE AS OUTLINED IMMEDIATELY BELOW: 
@@ -32,27 +32,27 @@ namespace Njb
     public class DeviceId
     {
         private static DeviceId [] device_id_list = {
-            //           Full Device Name                            Display/Short Name    Vendor ID   Product ID
-            new DeviceId("Creative Nomad Jukebox",                   "Nomad Jukebox",      0x0471,     0x0222),
-            new DeviceId("Creative Nomad Jukebox 2",                 "Nomad Jukebox",      0x041e,     0x4100),
-            new DeviceId("Creative Nomad Jukebox 3",                 "Nomad Jukebox",      0x041e,     0x4101),
-            new DeviceId("Creative Nomad Jukebox Zen",               "Zen Jukebox",        0x041e,     0x4108),
-            new DeviceId("Creative Nomad Jukebox Zen USB 2.0",       "Zen Jukebox",        0x041e,     0x410b),
-            new DeviceId("Creative Nomad Jukebox Zen NX",            "Zen NX Jukebox",     0x041e,     0x4109),
-            new DeviceId("Creative Nomad Jukebox Zen Xtra",          "Zen Xtra Jukebox",   0x041e,     0x4110),
-            new DeviceId("Dell Digital Jukebox",                     "Dell Jukebox",       0x041e,     0x4111),
-            new DeviceId("Creative Nomad Jukebox Zen Touch",         "Nomad Jukebox",      0x041e,     0x411b),
-            new DeviceId("Creative Zen (Zen Micro variant)",         "Zen Micro",          0x041e,     0x411d),
-            new DeviceId("Creative Nomad Jukebox Zen Micro",         "Zen Micro",          0x041e,     0x411e),
-            new DeviceId("Second Generation Dell Digital Jukebox",   "Dell Jukebox",       0x041e,     0x4126),
-            new DeviceId("Dell Pocket DJ",                           "Dell Pocket DJ",     0x041e,     0x4127),
-            new DeviceId("Creative Zen Sleek",                       "Zen Sleek",          0x041e,     0x4136)
+            //           Full Device Name                            Display/Short Name    Vendor ID   Product ID   Output formats                  Input Formats 
+            new DeviceId("Creative Nomad Jukebox",                   "Nomad Jukebox",      0x0471,     0x0222,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox 2",                 "Nomad Jukebox",      0x041e,     0x4100,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox 3",                 "Nomad Jukebox",      0x041e,     0x4101,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen",               "Zen Jukebox",        0x041e,     0x4108,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen USB 2.0",       "Zen Jukebox",        0x041e,     0x410b,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen NX",            "Zen NX Jukebox",     0x041e,     0x4109,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen Xtra",          "Zen Xtra Jukebox",   0x041e,     0x4110,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Dell Digital Jukebox",                     "Dell Jukebox",       0x041e,     0x4111,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen Touch",         "Nomad Jukebox",      0x041e,     0x411b,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Zen (Zen Micro variant)",         "Zen Micro",          0x041e,     0x411d,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Nomad Jukebox Zen Micro",         "Zen Micro",          0x041e,     0x411e,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Second Generation Dell Digital Jukebox",   "Dell Jukebox",       0x041e,     0x4126,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Dell Pocket DJ",                           "Dell Pocket DJ",     0x041e,     0x4127,      new string [] { "audio/mpeg" }, null),
+            new DeviceId("Creative Zen Sleek",                       "Zen Sleek",          0x041e,     0x4136,      new string [] { "audio/mpeg" }, null)
         };
+        
+        private static string [] EmptyArray = new string [] { };
 
         public static DeviceId [] ListAll {
-            get {
-                return device_id_list;
-            }
+            get { return device_id_list; }
         }
         
         public static bool IsNjbDevice(short vendorId, short productId)
@@ -75,37 +75,42 @@ namespace Njb
         private string display_name;
         private short vendor_id;
         private short product_id;
+        private string [] output_formats;
+        private string [] input_formats;
         
-        private DeviceId(string name, string displayName, short vendorId, short productId)
+        private DeviceId(string name, string displayName, short vendorId, short productId, 
+            string [] outputFormats, string [] inputFormats)
         {
             this.name = name;
             this.display_name = displayName;
             this.vendor_id = vendorId;
             this.product_id = productId;
+            this.output_formats = outputFormats;
+            this.input_formats = inputFormats;
         }
         
         public string Name {
-            get {
-                return name;
-            }
+            get { return name; }
         }
         
         public string DisplayName {
-            get {
-                return display_name;
-            }
+            get { return display_name; }
         }
         
         public short VendorId {
-            get {
-                return vendor_id;
-            }
+            get { return vendor_id; }
         }
         
         public short ProductId {
-            get {
-                return product_id;
-            }
+            get { return product_id; }
+        }
+        
+        public string [] OutputFormats {
+            get { return output_formats == null ? EmptyArray : output_formats; }
+        }
+        
+        public string [] InputFormats {
+            get { return input_formats == null ? EmptyArray : input_formats; }
         }
     }
 }
